9fc2de92d2cf7644469bb5bb6f3f40a7dafe1f83
Your Name <Your Email>
2022-10-12 22:56:10 +0530

    Step 07 - Updated

--- lib/widgets/dots_indicator.dart
+++ lib/widgets/dots_indicator.dart
@@ -2,14 +2,14 @@ import 'package:dots_indicator/dots_indicator.dart';
 import 'package:flutter/material.dart';
 
 class DotsIndicatorWidget extends StatelessWidget {
+  final double scrollPosition;
+  final int dots;
+
   const DotsIndicatorWidget({
     Key? key,
     required this.scrollPosition,
     required this.dots,
   }) : super(key: key);
-
-  final double scrollPosition;
-  final int dots;
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +19,7 @@ class DotsIndicatorWidget extends StatelessWidget {
       decorator: const DotsDecorator(
         color: Colors.grey,
         activeColor: Color.fromARGB(255, 33, 53, 167),
-        spacing: EdgeInsets.all(8),
+        spacing: EdgeInsets.all(8.0),
         size: Size.square(6),
         activeSize: Size(12, 6),
       ),
